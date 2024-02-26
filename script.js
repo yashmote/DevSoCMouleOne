e192ad3bb1448de7339ba460c4e75d827b4183ed
Yashvardhan Mote <90597471+yashmote@users.noreply.github.com>
2024-02-26 16:53:25 +0530

    script.js

--- script.js
+++ script.js
@@ -1,43 +1,24 @@
-const display = document.querySelector(".display");
-display.focus();
-const buttons = document.querySelectorAll("button");
-let output = "";
-
-document.addEventListener("keydown", () => {
-  output = display.value;
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        resultCal();
+    } else if (event.key === 'Backspace') {
+        form['input-text-field'].value = form['input-text-field'].value.slice(0, -1);
+    } else if (event.key.match(/[0-9+\-*/.=]/)) {
+        form['input-text-field'].value += event.key;
+    } else if (event.key === 'Escape') {
+        // Clear both input field and result display
+        acAllClear();
+    }
 });
 
-const calculate = (btnValue) => {
-  display.focus();
-  if ((btnValue === "=" && output !== "")) {
-    output = eval(output.replace("%", "/100"));
-  } 
-  else if (btnValue === "AC") {
-    output = "";
-  } 
-  else if (btnValue === "DEL") {
-    output = output.toString().slice(0, -1);
-  } 
-  else {
-    output += btnValue;
-  }
-  display.value = output;
-};
+function resultCal() {
+    let resultShow = document.querySelector('.input-result-show');
+    resultShow.innerHTML = eval(form['input-text-field'].value);
+    return false;
+}
 
-buttons.forEach((button) => {
-  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
-});
-
-document.addEventListener('keydown', (event) => {
-var name = event.key;
-var code = event.code;
-if (name === 'Enter') {
-    calculate('=')
+function acAllClear() {
+    document.querySelector('.input-result-show').innerHTML = '';
+    form['input-text-field'].value = '';
 }
-else if (name === 'Backspace') {
-    calculate('DEL')
-}
-else if (name === 'Escape') {
-    calculate('AC')
-}
-}, false);
